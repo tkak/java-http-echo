33b0b0cef56a39bd52e5e848ebeb68dec15cb814
Takaaki Furukawa <takaaki.frkw@gmail.com>
2020-08-15 22:32:52 +0900

    Initial commit

---
+++ src/main/java/com/tkak/javahttpecho/JavaHttpEchoApplication.java
@@ -0,0 +1,13 @@
+package com.tkak.javahttpecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHttpEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHttpEchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkak/javahttpecho/JavaHttpEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkak.javahttpecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHttpEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
